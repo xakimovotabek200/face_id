521ce3091be11b018467bef4296f4d8acf4c0818
ibrohim5009 <ibrohimabdurayimov5@gmail.com>
2023-07-07 11:05:09 +0500

    headshoot

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -16,16 +16,9 @@ export function AuthenticationTitle() {
         <Container size={420} my={40}>
             <Title
                 align="center"
-                sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
             >
-                Welcome back!
+                Welcome to Face Id Web Site!
             </Title>
-            <Text color="dimmed" size="sm" align="center" mt={5}>
-                Do not have an account yet?{' '}
-                <Anchor size="sm" component="button">
-                    Create account
-                </Anchor>
-            </Text>
 
             <Paper withBorder shadow="md" p={30} mt={30} radius="md">
                 <TextInput label="Your Name" placeholder="Name" required />
@@ -38,9 +31,13 @@ export function AuthenticationTitle() {
                         Forgot password?
                     </Anchor>
                 </Group>
-                <Button fullWidth mt="xl">
-                    Sign in
-                </Button>
+                <button
+                    type="button"
+                    class="border border-green-500 bg-green-500 text-white mx-auto rounded-md px-[138px] py-2 m-2 transition duration-500 ease select-none hover:bg-green-600 focus:outline-none focus:shadow-outline"
+                >
+                    Sing in
+                </button>
+
             </Paper>
         </Container>
     );
